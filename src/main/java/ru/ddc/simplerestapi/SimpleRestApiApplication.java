1394f0dc097a684b6d7c910a18f62aab0288c31a
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-28 22:13:03 +0300

    sra :: feat
    added checkout job

---
+++ src/main/java/ru/ddc/simplerestapi/SimpleRestApiApplication.java
@@ -0,0 +1,13 @@
+package ru.ddc.simplerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleRestApiApplication.class, args);
+    }
+
+}
