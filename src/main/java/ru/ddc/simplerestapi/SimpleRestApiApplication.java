cc3e4dd262eaee7ed1060f138e63d4be3163a8b9
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-29 00:32:44 +0300

    fix :: google-java-format

--- src/main/java/ru/ddc/simplerestapi/SimpleRestApiApplication.java
+++ src/main/java/ru/ddc/simplerestapi/SimpleRestApiApplication.java
@@ -12,10 +12,10 @@ public class SimpleRestApiApplication {
 
     /**
      * Main class of application.
+     *
      * @param args input arguments of application
      */
     public static void main(final String[] args) {
         SpringApplication.run(SimpleRestApiApplication.class, args);
     }
 }
-
