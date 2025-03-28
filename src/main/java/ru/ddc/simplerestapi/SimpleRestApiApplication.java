abe54ba5f693b87dac945da73b8f623c41c5bcfb
Sergey Kovalev <dipdeepcode@mail.ru>
2025-03-29 00:00:26 +0300

    fix :: java code style

--- src/main/java/ru/ddc/simplerestapi/SimpleRestApiApplication.java
+++ src/main/java/ru/ddc/simplerestapi/SimpleRestApiApplication.java
@@ -6,16 +6,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimpleRestApiApplication {
 
-    private SimpleRestApiApplication() {
-
-    }
-
     /**
      * Main class of application.
      * @param args input arguments of application
      */
     public static void main(final String[] args) {
         SpringApplication.run(SimpleRestApiApplication.class, args);
+    }
+
+    /**
+     * Fix Utility classes should not have a public or default constructor issue.
+     */
+    public void notForExecute() {
+        throw new RuntimeException();
     }
 
 }
